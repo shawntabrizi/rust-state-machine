29f233e8df22940b5c00655ec8bfcf0253add4ea
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-07-09 14:22:51 -0400

    solution: introduce the runtime to main

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,14 @@ mod system;
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 pub struct Runtime {
-	/* TODO:
-		- Create a field `system` which is of type `system::Pallet`.
-		- Create a field `balances` which is of type `balances::Pallet`.
-	*/
+	system: system::Pallet,
+	balances: balances::Pallet,
 }
 
 impl Runtime {
 	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
 	fn new() -> Self {
-		/* TODO: Create a new `Runtime` by creating new instances of `system` and `balances`. */
-		unimplemented!()
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
 	}
 }
 
