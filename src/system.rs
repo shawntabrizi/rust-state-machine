69385ae2148636c03804d4693467afef086e3d57
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-07-09 14:22:51 -0400

    template: add debug and print the runtime state

--- src/system.rs
+++ src/system.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeMap;
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
+/* TODO: Add the derive macro to implement the `Debug` trait for `Pallet`. */
 pub struct Pallet {
 	/// The current block number.
 	block_number: u32,
