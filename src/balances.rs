68fded83086af023107c88906bff07672f82ca82
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-07-12 12:52:02 -0400

    solution: add balances module state and initialization

--- src/balances.rs
+++ src/balances.rs
@@ -1,18 +1,16 @@
-/* TODO: Import `std::collections::BTreeMap` so it can be used below. */
+use std::collections::BTreeMap;
 
 /// This is the Balances Module.
 /// It is a simple module which keeps track of how much balance each account has in this state
 /// machine.
 pub struct Pallet {
 	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
-	/* TODO: Add a field `balances` which is a `BTreeMap` fom `String` to `u128`. */
+	balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
 	/// Create a new instance of the balances module.
 	pub fn new() -> Self {
-		/* TODO: Return a new instance of the `Pallet` struct. */
-		/* TODO: Remove `unimplemented!()`. */
-		unimplemented!()
+		Self { balances: BTreeMap::new() }
 	}
 }
