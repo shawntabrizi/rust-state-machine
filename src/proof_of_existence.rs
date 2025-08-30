03edfee05f761bd37dd82363d0bec5c0d1ac8028
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-08-30 09:13:45 +0800

    template: introduce proof of existence module

---
+++ src/proof_of_existence.rs
@@ -0,0 +1,25 @@
+use core::fmt::Debug;
+use std::collections::BTreeMap;
+
+pub trait Config: crate::system::Config {
+	/// The type which represents the content that can be claimed using this pallet.
+	/// Could be the content directly as bytes, or better yet the hash of that content.
+	/// We leave that decision to the runtime developer.
+	type Content: Debug + Ord;
+}
+
+/// This is the Proof of Existence Module.
+/// It is a simple module that allows accounts to claim existence of some data.
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	/// A simple storage map from content to the owner of that content.
+	/// Accounts can make multiple different claims, but each claim can only have one owner.
+	/* TODO: Add a field `claims` which is a `BTreeMap` fom `T::Content` to `T::AccountId`. */
+}
+
+impl<T: Config> Pallet<T> {
+	/// Create a new instance of the Proof of Existence Module.
+	pub fn new() -> Self {
+		/* TODO: Return a new instance of the `Pallet` struct. */
+	}
+}
