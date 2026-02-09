e4dbad2949696648b71c156c4ff637b690e0d063
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-09 13:55:39 +0000

    template: Interacting with Balances

--- src/balances.rs
+++ src/balances.rs
@@ -13,4 +13,17 @@ impl Pallet {
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
 	}
+
+	/// Set the balance of an account `who` to some `amount`.
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		/* Insert `amount` into the BTreeMap under `who`. */
+		unimplemented!()
+	}
+
+	/// Get the balance of an account `who`.
+	/// If the account has no stored balance, we return zero.
+	pub fn balance(&self, who: &String) -> u128 {
+		/* Return the balance of `who`, returning zero if `None`. */
+		unimplemented!()
+	}
 }
