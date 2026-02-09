61c1bf47859a8205f2ff329b5ae93034b4a530ff
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-09 13:55:41 +0000

    solution: Make System Configurable

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ mod types {
 	pub type Nonce = u32;
 }
 
-/*
-	TODO:
-	Implement the `system::Config` trait you created on your `Runtime`.
-	Use `Self` to satisfy the generic parameter required for `system::Pallet`.
-*/
-
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 #[derive(Debug)]
 pub struct Runtime {
-	system: system::Pallet<types::AccountId, types::BlockNumber, types::Nonce>,
+	system: system::Pallet<Self>,
 	balances: balances::Pallet<types::AccountId, types::Balance>,
+}
+
+impl system::Config for Runtime {
+	type AccountId = types::AccountId;
+	type BlockNumber = types::BlockNumber;
+	type Nonce = types::Nonce;
 }
 
 impl Runtime {
