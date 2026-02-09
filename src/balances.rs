ae73c99ddb11db6f5e010b91b08a28982cdf369b
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-09 13:55:39 +0000

    template: Adding State to Our Pallet

--- src/balances.rs
+++ src/balances.rs
@@ -1,1 +1,18 @@
-pub struct Pallet {}
+/* TODO: Import `std::collections::BTreeMap` so it can be used below. */
+
+/// This is the Balances Module.
+/// It is a simple module which keeps track of how much balance each account has in this state
+/// machine.
+pub struct Pallet {
+	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
+	/* TODO: Add a field `balances` which is a `BTreeMap` fom `String` to `u128`. */
+}
+
+impl Pallet {
+	/// Create a new instance of the balances module.
+	pub fn new() -> Self {
+		/* TODO: Return a new instance of the `Pallet` struct. */
+		/* TODO: Remove `unimplemented!()`. */
+		unimplemented!()
+	}
+}
