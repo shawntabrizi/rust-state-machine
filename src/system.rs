322403d86e482e239961bd7c4839a51758614695
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-27 07:38:32 +0000

    solution: Introduce the System Pallet

--- src/system.rs
+++ src/system.rs
@@ -1,17 +1,17 @@
-/* TODO: You might need to update your imports. */
+use std::collections::BTreeMap;
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
 pub struct Pallet {
 	/// The current block number.
-	/* TODO: Create a field `block_number` that stores a `u32`. */
+	block_number: u32,
 	/// A map from an account to their nonce.
-	/* TODO: Create a field `nonce` that is a `BTreeMap` from `String` to `u32`. */
+	nonce: BTreeMap<String, u32>,
 }
 
 impl Pallet {
 	/// Create a new instance of the System Pallet.
 	pub fn new() -> Self {
-		/* TODO: Return a new instance of the `Pallet` struct. */
+		Self { block_number: 0, nonce: BTreeMap::new() }
 	}
 }
