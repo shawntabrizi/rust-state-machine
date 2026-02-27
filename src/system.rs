b06ecde572b22556add1aeac9494afb76254ee05
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-27 07:38:32 +0000

    template: Making Your System Functional

--- src/system.rs
+++ src/system.rs
@@ -14,4 +14,39 @@ impl Pallet {
 	pub fn new() -> Self {
 		Self { block_number: 0, nonce: BTreeMap::new() }
 	}
+
+	/// Get the current block number.
+	pub fn block_number(&self) -> u32 {
+		/* TODO: Return the current block number. */
+		unimplemented!()
+	}
+
+	// This function can be used to increment the block number.
+	// Increases the block number by one.
+	pub fn inc_block_number(&mut self) {
+		/* TODO: Increment the current block number by one. */
+		unimplemented!()
+	}
+
+	// Increment the nonce of an account. This helps us keep track of how many transactions each
+	// account has made.
+	pub fn inc_nonce(&mut self, who: &String) {
+		/* TODO: Get the current nonce of `who`, and increment it by one. */
+		unimplemented!()
+	}
+}
+
+#[cfg(test)]
+mod test {
+	#[test]
+	fn init_system() {
+		/* TODO: Create a test which checks the following:
+			- Increment the current block number.
+			- Increment the nonce of `alice`.
+
+			- Check the block number is what we expect.
+			- Check the nonce of `alice` is what we expect.
+			- Check the nonce of `bob` is what we expect.
+		*/
+	}
 }
