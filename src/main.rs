b0603f92861819d21efc1ccdfe9183e1cfa71626
gitorial-cli <gitorial-cli@users.noreply.github.com>
2026-02-27 07:38:32 +0000

    template: Creating Our Runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod balances;
 mod system;
 
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+	/* TODO:
+		- Create a field `system` which is of type `system::Pallet`.
+		- Create a field `balances` which is of type `balances::Pallet`.
+	*/
+}
+
+impl Runtime {
+	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
+	fn new() -> Self {
+		/* TODO: Create a new `Runtime` by creating new instances of `system` and `balances`. */
+		unimplemented!()
+	}
+}
+
 fn main() {
 	println!("Hello, world!");
 }
